fe5486be18d946cdd3e2ce52eaea00a6c01cb1fd
coderygor <ygorpereira@indecx.com.br>
2022-05-12 11:40:34 -0300

    create: with sequelize

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'mssql',
+    host: 'localhost',
+    username: 'dev',
+    passoword: '',
+    database: 'master',
+    define: {
+        timestamps:true,
+        underscored:true,
+    },
+};
+
+
